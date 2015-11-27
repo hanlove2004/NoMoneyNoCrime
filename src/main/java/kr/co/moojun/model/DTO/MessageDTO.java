bb71964d0289a09d52bdf7ca56b5c82023656080
dnaudwp12 <dnaudwp123@naver.com>
2015-11-27 17:08:47 +0900

    DTO

--- src/main/java/kr/co/moojun/model/DTO/MessageDTO.java
+++ src/main/java/kr/co/moojun/model/DTO/MessageDTO.java
@@ -1,5 +1,44 @@
 package kr.co.moojun.model.DTO;
 
-public class MessageDTO {
+import java.sql.Date;
 
+public class MessageDTO {
+	private int num;
+	private String sender;
+	private String receiver;
+	private String content;
+	private Date regdate;
+	
+	public int getNum() {
+		return num;
+	}
+	public void setNum(int num) {
+		this.num = num;
+	}
+	public String getSender() {
+		return sender;
+	}
+	public void setSender(String sender) {
+		this.sender = sender;
+	}
+	public String getReceiver() {
+		return receiver;
+	}
+	public void setReceiver(String receiver) {
+		this.receiver = receiver;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public Date getRegdate() {
+		return regdate;
+	}
+	public void setRegdate(Date regdate) {
+		this.regdate = regdate;
+	}
+	
+	
 }
--- src/main/java/kr/co/moojun/model/DTO/NoticeboardDTO.java
+++ src/main/java/kr/co/moojun/model/DTO/NoticeboardDTO.java
@@ -1,5 +1,44 @@
 package kr.co.moojun.model.DTO;
 
-public class NoticeboardDTO {
+import java.sql.Date;
 
+public class NoticeboardDTO {
+	private int num;
+	private String id;
+	private String title;
+	private String content;
+	private Date regdate;
+	
+	public int getNum() {
+		return num;
+	}
+	public void setNum(int num) {
+		this.num = num;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public Date getRegdate() {
+		return regdate;
+	}
+	public void setRegdate(Date regdate) {
+		this.regdate = regdate;
+	}
+	
+	
 }
