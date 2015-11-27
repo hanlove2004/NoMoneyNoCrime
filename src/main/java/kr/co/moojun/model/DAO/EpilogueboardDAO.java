55c9b1ff23336d2fce1e53060d8707557efff95c
hanlove2004 <hanlove2004@naver.com>
2015-11-27 18:52:50 +0900

    여행후기 DAO 작성 (한금찬)

--- src/main/java/kr/co/moojun/model/DAO/EpilogueboardDAO.java
+++ src/main/java/kr/co/moojun/model/DAO/EpilogueboardDAO.java
@@ -1,5 +1,19 @@
 package kr.co.moojun.model.DAO;
 
-public interface EpilogueboardDAO {
+import java.util.HashMap;
+import java.util.List;
 
+import kr.co.moojun.model.DTO.EpilogueboardDTO;
+
+public interface EpilogueboardDAO {
+	
+	void insertEpilogue(EpilogueboardDTO dto); //쓰기
+	List<EpilogueboardDTO> getEpilogueList(HashMap map); //리스트(목록)
+	EpilogueboardDTO getEpilogue(int num); //디테일(상세보기)
+	
+	int updateEpilogue(EpilogueboardDTO dto); //여행후기 수정하기
+	int deleteEpilogue(EpilogueboardDTO dto); //여행후기 삭제하기
+	
+	int getEpilogueCount(); //여행후기 게시글의 수
+	
 }
