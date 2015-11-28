540466ca40d8f454f7a564e1e7147a025a8a1c43
hanlove2004 <hanlove2004@naver.com>
2015-11-28 10:54:08 +0900

    MemberDAO mapper, FreeboardDAO mapper, MatchingDAO mapper 작성 및 MemberDTO 수정 (한금찬)

--- src/main/java/kr/co/moojun/model/DTO/MemberDTO.java
+++ src/main/java/kr/co/moojun/model/DTO/MemberDTO.java
@@ -1,8 +1,11 @@
 package kr.co.moojun.model.DTO;
 
+import java.sql.Date;
+
 public class MemberDTO {
 	
 	private String id;
+	private int num;
 	private String name;
 	private String pwd;
 	private String birth;
@@ -10,12 +13,19 @@ public class MemberDTO {
 	private String phone;
 	private String email;
 	private String panalty;
+	private Date joindate;
 	
 	public String getId() {
 		return id;
 	}
 	public void setId(String id) {
 		this.id = id;
+	}
+	public int getNum() {
+		return num;
+	}
+	public void setNum(int num) {
+		this.num = num;
 	}
 	public String getName() {
 		return name;
@@ -58,6 +68,12 @@ public class MemberDTO {
 	}
 	public void setPanalty(String panalty) {
 		this.panalty = panalty;
+	}
+	public Date getJoindate() {
+		return joindate;
+	}
+	public void setJoindate(Date joindate) {
+		this.joindate = joindate;
 	}
 	
 }
