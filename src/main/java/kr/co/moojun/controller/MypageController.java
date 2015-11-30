cd0642c5c7ab5851d911e20884f65242220067f2
hanlove2004 <hanlove2004@naver.com>
2015-11-30 14:12:42 +0900

    한글 깨짐 테스트 (한금찬)

--- src/main/java/kr/co/moojun/controller/MypageController.java
+++ src/main/java/kr/co/moojun/controller/MypageController.java
@@ -1,12 +1,23 @@
 package kr.co.moojun.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class MypageController {
-	
-	String exam = "���ȣ";/* �׽�Ʈ */
-	
-	String exam2 = "hello 한글"; //한글테스트
+
+	String exam = "hello 한글"; // 한글테스트
+	String exam2 = "안녕하세요"; // 한글테스트2
+
+	// 마이페이지 회원정보 (memberinfo.htm)
+	@RequestMapping(value = "memberinfo.htm", method = RequestMethod.GET)
+	public String memberinfo() {
+
+		System.out.println("");
+
+		// Tiles 적용 (UrlBase 방식)
+		return "mypage.memberinfo";
+	}
 
 }
