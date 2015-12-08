5b0adbd354997a207c2c4df5b3cc134d8569f3ef
nam1554 <gisubsub@gamil.com>
2015-12-08 14:23:17 +0900

    스프링 스케줄러 test 추가(기섭)

---
+++ src/main/java/kr/co/moojun/controller/Scheduler.java
@@ -0,0 +1,14 @@
+package kr.co.moojun.controller;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Scheduler {
+/*
+	@Scheduled(cron = "0/5 * * * * ?")
+	public void crontest1(){
+		System.out.println("5초 마다");
+	}
+	*/
+}
